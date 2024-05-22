975663adbbead2bfa32de0c5179d5f3e97071fa4
Arifjawad18 <arif.jawad18@gmail.com>
2024-05-22 13:50:26 +0600

    des

--- destructuring.js
+++ destructuring.js
@@ -1,1 +1,13 @@
 // 1. array destructuring
+
+const student = {
+     name: "Sakib",
+     age: 32,
+     movies: ["King khan,Dhakar mastan"],
+   };
+
+// 1. template string 
+const about = `My Name is ${student.name} age of ${student.age} has number ${numbers[2]} also like movies ${student.movies[0]}`;
+console.log(about)
+
+// 2. arrow function
