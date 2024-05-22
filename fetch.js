060e153e96109c147641db06bafd786ecb2f2f66
Arifjawad18 <arif.jawad18@gmail.com>
2024-05-22 14:41:21 +0600

    JSON

--- fetch.js
+++ fetch.js
@@ -9,4 +9,15 @@ console.log(student);
 console.log(studentJSON);
 
 const studentObj = JSON.parse(studentJSON);
-const.log(studentObj);
+// console.log(studentObj);
+
+// 2.fetch
+fetch("url")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+// keys, values
+const numbers = [23, 32, 87, 56, 43];
+numbers, forEach((num) => console.log(num));
+numbers.map(num = num * 2);
+
