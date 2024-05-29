9a7a6a6817772210f7d18e886b7940fe7e7751a3
Arifjawad18 <arif.jawad18@gmail.com>
2024-05-30 00:10:57 +0600

    default

---
+++ default.js
@@ -0,0 +1,16 @@
+function add(num1, num2){
+     const result = num1 + num2;
+     console.log(num1,num2, result);
+     return result;
+}
+
+function fullName(first,last=''){
+     const full = first +''+ last;
+     return full;
+}
+function friends(numbs =[]){
+
+}
+function person(human = {}){
+     
+}
