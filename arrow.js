a9861c653c9ebddc166e606bb345e42f856caa07
Arifjawad18 <arif.jawad18@gmail.com>
2024-06-01 13:20:08 +0600

    arrow

---
+++ arrow.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  const result = a + b;
+  return result;
+}
+
+// arrow function
+const add3 = (a, b) => a + b;
+const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+const multiply = (num1, num2) => num1 * num2;
+
+const mult= multiply(3,12)
+console.log(mult); 
+
