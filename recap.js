faf64bf4e8fe9271b85e25c85f6984f25df59efe
Arifjawad18 <arif.jawad18@gmail.com>
2024-07-18 02:34:02 +0600

    recap es6

---
+++ recap.js
@@ -0,0 +1,8 @@
+// 1.var let const 
+// 2.default parameter
+// 3.template string
+// 4. arrow function
+// 5.destructuring and spread operator
+// 6object.key, object.values, object.entries
+// 7. for of used in Array and string,
+// 8.for in loop used in object
