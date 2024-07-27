193af57b357d02ec72274e443a3fc54e915a1d62
Arifjawad18 <arif.jawad18@gmail.com>
2024-07-27 21:23:29 +0600

    map es6 and reduce

--- map.js
+++ map.js
@@ -16,6 +16,32 @@
 
 
 // // more map // //
-const numbers = [12,10, 8,15,7];
-// console.log(doubled)
+// const numbers = [12,10, 8,15,7];
+// // console.log(doubled)
+// const doubled = numbers.map(num => num*2)
 
+// 1. map
+// 2. forEach
+// 3.filter
+// 4.find
+// 5. reduce
+
+// const numbers = [4,5,7,1,3,66];
+// const total = numbers.reduce((previous,current)=> previous+current,0)
+// console.log(total)
+
+
+ const products=[
+     {id:1,name:'lenovo', price:65000},
+     {id:1,name:'dell', price:22000},
+     {id:1,name:'hp', price:40000},
+     {id:1,name:'lenovo', price:74000},
+
+ ]
+//  map
+const names = products.map(product => product.name);
+console.log(names);
+
+// reduce
+const total = products.reduce((acum, current) => acum +current.price,0)
+console.log(total)
