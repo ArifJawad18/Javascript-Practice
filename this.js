997616467851c65f683a01ece4bc2416728b8210
Arifjawad18 <arif.jawad18@gmail.com>
2024-07-29 02:51:50 +0600

    this  es6

---
+++ this.js
@@ -0,0 +1,14 @@
+class Person {
+     constructor(name, age){
+          this.name = name;
+          this.age = age;
+     }
+
+sleep(){
+     console.log(`Sleeping now${this.name}`)
+
+}
+activity(){
+     this.sleep();
+}
+}
