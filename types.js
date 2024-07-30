42201e3499803feabbf17b04e9d7cb614b2b5a1b
Arifjawad18 <arif.jawad18@gmail.com>
2024-07-31 00:23:20 +0600

    this  and types es6

---
+++ types.js
@@ -0,0 +1,6 @@
+// dynamic typeof language
+const a = 5;
+const b = 'i know that' 
+const ages = [45,65,48];
+const student = {id:23, class:7}
+console.log(typeof a,typeof b, typeof ages, typeof student);
