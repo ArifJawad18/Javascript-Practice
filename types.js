76a89a63ec9e32c3376e17d366e3e97c67024d26
Arifjawad18 <arif.jawad18@gmail.com>
2024-07-31 15:19:40 +0600

    scopejs es6

--- types.js
+++ types.js
@@ -1,6 +1,19 @@
-// dynamic typeof language
-const a = 5;
-const b = 'i know that' 
-const ages = [45,65,48];
-const student = {id:23, class:7}
-console.log(typeof a,typeof b, typeof ages, typeof student);
+// // dynamic typeof language
+// const a = 5;
+// const b = 'i know that' 
+// const ages = [45,65,48];
+// const student = {id:23, class:7}
+// console.log(typeof a,typeof b, typeof ages, typeof student);
+
+
+
+//double equal (==) vs triple equal (===), implicit conversion //
+const first =[45,54,98];
+const second = [45,54 ,98]
+if(first === second){
+     console.log('values are eqal')
+}
+else{
+     console.log('values are not equal')
+}
+
