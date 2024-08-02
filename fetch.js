ef11fe66f6e6dd480be5a95973f7db45ba1803e9
Arifjawad18 <arif.jawad18@gmail.com>
2024-08-03 03:23:39 +0600

    api json

--- fetch.js
+++ fetch.js
@@ -1,34 +1,3 @@
-// 1 JSON
-const student = {
-  name: "salib khan",
-  age: 32,
-  movies: ["King khan,Dhakar mastan"],
-};
-const studentJSON = JSON.stringify(student);
-console.log(student);
-console.log(studentJSON);
-
-const studentObj = JSON.parse(studentJSON);
-// console.log(studentObj);
-
-// 2.fetch
-fetch("url")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
-
-// keys, values
-const numbers = [23, 32, 87, 56, 43];
-numbers, forEach((num) => console.log(num));
-numbers.map(num = num * 2);
-
-//  ///// ///// /////// ///// /////
-//  class Vahicle{
-//   constructor(name, price){
-//     this.name=name;
-//     this.price=price;
-
-//   }
-//   move(){
-//     console.log('we will go out side')
-//   }
-//  }
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
