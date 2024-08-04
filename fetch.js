ad0de4474502540b2ee2de6be6561f06d32cbff8
Arifjawad18 <arif.jawad18@gmail.com>
2024-08-04 23:14:47 +0600

    fetch

--- fetch.js
+++ fetch.js
@@ -4,10 +4,11 @@
 */
 
 function loadData(){
-  const url = 'https://jsonplaceholder.typicode.com/todos/1'
+  const url = 'https://jsonplaceholder.typicode.com/users'
   fetch(url) 
   .then(response => response.json())
   .then(json => console.log(json))
 
 
 }
+
