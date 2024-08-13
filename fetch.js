75e94d58996a6739bce917898ec3ead168a08682
Arifjawad18 <arif.jawad18@gmail.com>
2024-08-13 15:52:23 +0600

    fetch

--- fetch.js
+++ fetch.js
@@ -13,13 +13,3 @@ function loadData(data) {
     console.log(user.email);
   }
 }
-function loadData() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => loadData(data));
-}
-function loadData(data) {
-  for (const user of data) {
-    console.log(user.email);
-  }
-}
