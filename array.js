b3869e17faccdd494709372d0590a21b1e9d9c23
Arifjawad18 <arif.jawad18@gmail.com>
2024-08-23 02:56:15 +0600

    array

--- array.js
+++ array.js
@@ -2,14 +2,15 @@
 //   const result = a + b;
 //   return result;
 // }
-
 // // arrow function
 // const add3 = (a, b) => a + b;
 // const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
 // const multiply = (num1, num2) => num1 * num2;
-
 // const multi= multiply(3,12)
-// console.log(mult); 
+// console.log(mult);
 
-const number = [12,14,44,56]
-const friend = ["me","you","i","he","she"];
+/////  Array length, index, Get and set by index ////////
+const number = [12, 14, 44, 56];
+const friend = ["me", "you", "i", "he", "she"];
+console.log(number);
+console.log(number[1]);
