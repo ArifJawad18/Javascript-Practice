af8b76eae40ba09238c0dbb5604e1f1257d5c9e2
Arifjawad18 <arif.jawad18@gmail.com>
2024-08-23 02:48:42 +0600

    array

--- arrow.js
+++ arrow.js
@@ -1,13 +1,15 @@
-function add(a, b) {
-  const result = a + b;
-  return result;
-}
+// function add(a, b) {
+//   const result = a + b;
+//   return result;
+// }
 
-// arrow function
-const add3 = (a, b) => a + b;
-const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
-const multiply = (num1, num2) => num1 * num2;
+// // arrow function
+// const add3 = (a, b) => a + b;
+// const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+// const multiply = (num1, num2) => num1 * num2;
 
-const multi= multiply(3,12)
-console.log(mult); 
+// const multi= multiply(3,12)
+// console.log(mult); 
 
+const number = [12,14,44,56]
+const friend = ["me","you","i","he","she"];
