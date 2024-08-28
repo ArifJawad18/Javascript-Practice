16530c32b9b31d1e0c5246fb85ac67a31d1131d7
Arifjawad18 <arif.jawad18@gmail.com>
2024-08-29 00:35:11 +0600

    loop

--- loop.js
+++ loop.js
@@ -97,16 +97,8 @@ for(let num=0 ;num <5; num++){
   console.log(num);
 
 }
-for(let num=0 ;num <5; num++){
-  console.log(num);
 
-}
-for(let num=0 ;num <5; num++){
-  console.log(num);
 
-}
-for(let num=0 ;num <5; num++){
-  console.log(num);
 
-}
+
 
