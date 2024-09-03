3eeb9a76d2621906389385af7776ce9ac27bd630
Arifjawad18 <arif.jawad18@gmail.com>
2024-09-04 01:18:41 +0600

    continue

--- loop.js
+++ loop.js
@@ -100,3 +100,15 @@ for(let i =11; i<=20; i++){
 }
 
 
+// for(let i =0; i <20;i++){
+//   if(i% 2 !==0){
+//     console.log(i)
+//   }
+// }
+
+for(let i =1; i< 20; i+=2){
+  console.log(i);
+}
+
+
+
