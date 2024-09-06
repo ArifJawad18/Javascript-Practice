9776d183be0db214f20b80bbb57f1ceb812ad128
Arifjawad18 <arif.jawad18@gmail.com>
2024-09-07 02:38:06 +0600

    string

--- string.js
+++ string.js
@@ -1,1 +1,10 @@
-const country = 'Bang'
+const country = 'Bangladesh';
+
+const divison = "Fani"
+
+const number =[33,44,56,78,];
+
+console.log(number.length);
+console.log(number[1]);
+console.log(capital[2]);
+
