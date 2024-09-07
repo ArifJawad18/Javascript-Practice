5eecf968f4f6343c427f79fedcc3cb2417d50a44
Arifjawad18 <arif.jawad18@gmail.com>
2024-09-08 00:18:48 +0600

    string

--- string.js
+++ string.js
@@ -1,10 +1,21 @@
-const country = 'Bangladesh';
+// const country = 'Bangladesh';
 
-const divison = "Fani"
+// const divison = "Fani"
 
-const number =[33,44,56,78,];
+// const number =[33,44,56,78,];
 
-console.log(number.length);
-console.log(number[1]);
-console.log(capital[2]);
+// console.log(number.length);
+// console.log(number[1]);
+// console.log(capital[2]);
 
+
+// const  address = 'andorKilla';
+// const part = address.slice()
+
+
+const sentence  = "I am learning web dev"
+
+let reverse =''
+for(const letter of sentence){
+     console.log(letter);
+}
