b233b0cff34031c323ae5261194da6996a2272a6
Arifjawad18 <arif.jawad18@gmail.com>
2024-09-17 23:50:40 +0600

    return

--- loop.js
+++ loop.js
@@ -119,3 +119,124 @@ for(let i =11; i<=20; i++){
 
 
 
+// const salary = 25000;
+// const isBCS = true;
+
+// if (salary > 20000 && height > 66) {
+//   console.log("yes do this");
+// } else {
+//   console.log("no dont do this");
+// }
+
+// // MULTI_LEVEL CONDITION
+
+// const price = 5000;
+
+// if (price >= 5000) {
+//   // 10% discount
+//   const discount = (price * 10) / 100;
+//   const payAmount = price - discount;
+//   console.log(payAmount);
+// } else {
+//   console.log(price);
+// }
+
+///// Nested if-else condition//////////////
+
+// const money = 500;
+// if (money > 300) {
+//   console.log("i know you you are rich");
+// } else {
+//   if (money > 100) {
+//     console.log("no i am not rich");
+//   }
+// }
+
+//////If-else shorthand Ternary Operator//////////
+
+// simple ternary
+// age>=18? console.log('ha dio'): console.log('ghumai thako');
+
+// let price = 500;
+// const isLeader = true;
+// if(isLeader === true){
+//   price=0;
+// }
+// else{
+//   price= price +100;
+// }
+// console.log()
+
+// const number =7;
+// if(number<10){
+//   console.log('number pelam')
+// }
+// console.log('something new is cooking')
+
+// // ---- WHILE---lOOP------------
+
+// let num = 0;
+// while(num < 5){
+//   console.log('looping,num');
+//   // num = num + 1;
+//   // num +=1;
+//   num++;
+// }
+
+//   let num = 1;
+//   let sum = 0;
+//   while (num <= 10){
+//     console.log(num);
+//     sum = sum+num;
+//     console.log('sum:', sum);
+//     num++;
+//   }
+
+
+// // Array 
+// let num = 1;
+// while(num <= 10){
+//   console.log(num);
+//   num++;
+
+// }
+
+// For Loop // 
+// let num = 0;
+// while(num < 5){
+//   console.log(num);
+//   num++;
+// }
+
+// for(let num=0 ;num <5; num++){
+//   console.log(num);
+
+// }
+
+// /// //for sum// /////
+let sum = 0;
+for(let i =11; i<=20; i++){
+  sum = sum +i;
+  console.log(i)
+}
+
+
+// for(let i =0; i <20;i++){
+//   if(i% 2 !==0){
+//     console.log(i)
+//   }
+// }
+
+// for(let i =1; i< 20; i+=2){
+//   console.log(i);
+// }
+// const friend = ['Elon','Mark','Bill'];
+
+
+// const number = [50,23,456,222,345,323]
+// for (let i =0; i<number.length; i++){
+
+// }
+
+
+
