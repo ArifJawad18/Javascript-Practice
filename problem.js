a1f53bc423067e9b502cfee3f2915441fdd03494
Arifjawad18 <arif.jawad18@gmail.com>
2024-09-22 01:07:53 +0600

    problem solve

--- problem.js
+++ problem.js
@@ -1,18 +1,32 @@
-function isLeapYear (year){
-     if(year % 4===0){
-          return true;
-     }
-     else{
-          return false;
-     }
-}
-const isLipi = isLeapYear(2052);
-console.log(isLipi);
+// function isLeapYear (year){
+//      if(year % 4===0){
+//           return true;
+//      }
+//      else{
+//           return false;
+//      }
+// }
+// const isLipi = isLeapYear(2052);
+// console.log(isLipi);
 
-function addAverage(number ){
+// function addAverage(number ){
 
-}
-const numbers = [42.33,54,65,32,54];
+// }
+// const numbers = [42.33,54,65,32,54];
 
-const avg = addAverage(numbers);
-console.log('average of number odd numbers:',avg);
+// const avg = addAverage(numbers);
+// console.log('average of number odd numbers:',avg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
