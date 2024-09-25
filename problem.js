fd5c7a4f30868d05bf83e7c022b7f194342d1db7
Arifjawad18 <arif.jawad18@gmail.com>
2024-09-25 15:26:19 +0600

    calculate

--- problem.js
+++ problem.js
@@ -22,7 +22,6 @@
 
 // const names =  [aiul, yoko,adel, formil,nami]
 
-
 // Swap variable, swap without temp, destructing
 
 // let a= 7 ;
@@ -30,11 +29,26 @@
 // console.log(a,b);
 
 // ////////////////////////////////
-console.log(Math.max(1, 3, 2));
+// console.log(Math.max(1, 3, 2));
 
-const max = Math.max(12,1,56,44,66,96,4);
-console.log('max isuing Math.max',max);
+// const max = Math.max(12,1,56,44,66,96,4);
+// console.log('max isuing Math.max',max);
 
+// ///////////////multiplication to calculate ///////////////////
 
+function woodQuantity(chair, table, bed) {
+  const perChairWood = 3;
+  const perTableWood = 10;
+  const perBedWood = 50;
 
+  const chairTotalWood = chairQuantity * perChairWood;
+  const tableTotaleWood = tableTotaleWood * perTableWood;
+  const bedTotalWood = bedQuantity * perBedWood;
 
+  const totalWood = chairTotalWood + tableTotaleWood + bedTotalWood;
+
+  return totalWood;
+}
+
+const wood = woodQuantity(0, 0, 1);
+console.log("wood needed", wood);
