52832b14c71a1d05d61367f2e2286e086bb3ea94
Arifjawad18 <arif.jawad18@gmail.com>
2025-06-06 20:53:13 +0600

    template

--- loop.js
+++ loop.js
@@ -1,35 +1,35 @@
 // const salary = 25000;
 // const isBCS = true;
 
-// if (salary > 20000 && height > 66) {
-//   console.log("yes do this");
-// } else {
-//   console.log("no dont do this");
-// }
+if (salary > 20000 && height > 66) {
+  console.log("yes do this");
+} else {
+  console.log("no dont do this");
+}
 
 // // MULTI_LEVEL CONDITION
 
 // const price = 5000;
 
-// if (price >= 5000) {
-//   // 10% discount
-//   const discount = (price * 10) / 100;
-//   const payAmount = price - discount;
-//   console.log(payAmount);
-// } else {
-//   console.log(price);
-// }
+if (price >= 5000) {
+  // 10% discount
+  const discount = (price * 10) / 100;
+  const payAmount = price - discount;
+  console.log(payAmount);
+} else {
+  console.log(price);
+}
 
 ///// Nested if-else condition//////////////
 
-// const money = 500;
-// if (money > 300) {
-//   console.log("i know you you are rich");
-// } else {
-//   if (money > 100) {
-//     console.log("no i am not rich");
-//   }
-// }
+const money = 500;
+if (money > 300) {
+  console.log("i know you you are rich");
+} else {
+  if (money > 100) {
+    console.log("no i am not rich");
+  }
+}
 
 //////If-else shorthand Ternary Operator//////////
 
@@ -91,13 +91,6 @@
 //   console.log(num);
 
 // }
-
-// /// //for sum// /////
-let sum = 0;
-for(let i =11; i<=20; i++){
-  sum = sum +i;
-  console.log(i)
-}
 
 
 // for(let i =0; i <20;i++){
@@ -175,13 +168,13 @@ for(let i =11; i<=20; i++){
 
 // // ---- WHILE---lOOP------------
 
-// let num = 0;
-// while(num < 5){
-//   console.log('looping,num');
-//   // num = num + 1;
-//   // num +=1;
-//   num++;
-// }
+let num = 0;
+while(num < 5){
+  console.log('looping,num');
+  // num = num + 1;
+  // num +=1;
+  num++;
+}
 
 //   let num = 1;
 //   let sum = 0;
--- template.js
+++ template.js
@@ -2,3 +2,8 @@ const first = 'you'
 const last = 'me'
 const greet = 'ptashiyam'
 
+
+let firstName = "John";
+let lastName = "Doe";
+
+let text = `Welcome ${firstName}, ${lastName}!`;
